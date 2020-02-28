0a81d075148ee63fc9f666bfaab18735b8ff8d1b
Reece Chen <esawei5190@gmail.com>
2020-02-28 13:41:20 +0800

    [refactor][select]convert JoeySelect to extension method

--- CSharpAdvanceDesignTests/JoeySelectTests.cs
+++ CSharpAdvanceDesignTests/JoeySelectTests.cs
@@ -5,6 +5,7 @@ using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
 using System.Linq;
+using Lab;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -16,7 +17,7 @@ namespace CSharpAdvanceDesignTests
         {
             var urls = GetUrls();
 
-            var actual = JoeySelect(urls, url => url.Replace("http:", "https:"));
+            var actual = urls.JoeySelect(url => url.Replace("http:", "https:"));
             var expected = new List<string>
             {
                 "https://tw.yahoo.com",
@@ -33,7 +34,7 @@ namespace CSharpAdvanceDesignTests
         {
             var urls = GetUrls();
 
-            var actual = JoeySelect(urls, url => $"{url}:9191");
+            var actual = urls.JoeySelect(url => $"{url}:9191");
             var expected = new List<string>
             {
                 "http://tw.yahoo.com:9191",
@@ -55,7 +56,7 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "David", LastName = "Chen"}
             };
 
-            var names = JoeySelect(employees, e => $"{e.FirstName} {e.LastName}");
+            var names = employees.JoeySelect(e => $"{e.FirstName} {e.LastName}");
             var expected = new[]
             {
                 "Joey Chen",
@@ -63,17 +64,6 @@ namespace CSharpAdvanceDesignTests
                 "David Chen",
             };
             expected.ToExpectedObject().ShouldMatch(names);
-        }
-
-        private IEnumerable<TResult> JoeySelect<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
-        {
-            var result = new List<TResult>();
-            foreach (var item in source)
-            {
-                result.Add(selector(item));
-            }
-
-            return result;
         }
 
         private static IEnumerable<string> GetUrls()
