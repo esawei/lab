4def907fc1fcc45bae96a68e65263da859598e6e
Reece Chen <esawei5190@gmail.com>
2020-02-28 10:00:16 +0800

    [refactor] inline variable

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -52,8 +52,7 @@ namespace CSharpAdvanceDesignTests
                 new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
             };
 
-            Predicate<Product> predicate = product => product.Price > 200 && product.Price < 500 && product.Cost < 30;
-            var actual = JoeyWhere(products, predicate);
+            var actual = JoeyWhere(products, product => product.Price > 200 && product.Price < 500 && product.Cost < 30);
 
             var expected = new List<Product>
             {
