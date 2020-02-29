7b58dca2d9f7161b651e966263daa3b19c6c4c5f
Reece Chen <esawei5190@gmail.com>
2020-02-29 11:00:52 +0800

    [feat][first] pass test case: get_first_girl

--- CSharpAdvanceDesignTests/JoeyFirstTests.cs
+++ CSharpAdvanceDesignTests/JoeyFirstTests.cs
@@ -1,4 +1,5 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
 using System.Collections.Generic;
@@ -6,7 +7,6 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyFirstTests
     {
         [Test]
@@ -14,20 +14,27 @@ namespace CSharpAdvanceDesignTests
         {
             var girls = new[]
             {
-                new Girl(){Age = 10},
+                new Girl(){Age = 60},
                 new Girl(){Age = 20},
                 new Girl(){Age = 30},
             };
 
             var girl = JoeyFirst(girls);
-            var expected = new Girl { Age = 10 };
+            var expected = new Girl { Age = 60 };
 
             expected.ToExpectedObject().ShouldEqual(girl);
         }
 
-        private Girl JoeyFirst(IEnumerable<Girl> girls)
+        private TSource JoeyFirst<TSource>(IEnumerable<TSource> girls)
         {
-            throw new System.NotImplementedException();
+            var enumerator = girls.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                return current;
+            }
+
+            throw new InvalidOperationException(); 
         }
     }
 }
