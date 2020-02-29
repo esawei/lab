d457a817cd3bfd7bba25403e70b8c2f8600763d0
Reece Chen <esawei5190@gmail.com>
2020-02-29 11:20:49 +0800

    [feat][first]pass test case: get_first_chen

--- CSharpAdvanceDesignTests/JoeyFirstTests.cs
+++ CSharpAdvanceDesignTests/JoeyFirstTests.cs
@@ -3,7 +3,6 @@ using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
 using System.Collections.Generic;
-using Lab;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -15,13 +14,13 @@ namespace CSharpAdvanceDesignTests
         {
             var girls = new[]
             {
-                new Girl(){Age = 60},
-                new Girl(){Age = 20},
-                new Girl(){Age = 30},
+                new Girl() {Age = 60},
+                new Girl() {Age = 20},
+                new Girl() {Age = 30},
             };
 
-            var girl = girls.JoeyFirst();
-            var expected = new Girl { Age = 60 };
+            var girl = JoeyFirst(girls);
+            var expected = new Girl {Age = 60};
 
             expected.ToExpectedObject().ShouldEqual(girl);
         }
@@ -34,7 +33,47 @@ namespace CSharpAdvanceDesignTests
             {
             };
 
-            Assert.Catch<InvalidOperationException>(() => girls.JoeyFirst());
+            Assert.Catch<InvalidOperationException>(() => JoeyFirst(girls));
+        }
+
+        [Test]
+        public void get_first_chen()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "David", LastName = "Chen"}
+            };
+            var employee = JoeyFirst(employees, employee1 => employee1.LastName == "Chen");
+            new Employee() {FirstName = "Joey", LastName = "Chen"}.ToExpectedObject().ShouldMatch(employee);
+        }
+
+        private TSource JoeyFirst<TSource>(IEnumerable<TSource> sources, Func<TSource, bool> predicate)
+        {
+            var enumerator = sources.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var employee = enumerator.Current;
+                if (predicate(employee))
+                {
+                    return employee;
+                }
+            }
+
+            throw new InvalidOperationException($"{nameof(sources)} is empty");
+        }
+
+        private TSource JoeyFirst<TSource>(IEnumerable<TSource> sources)
+        {
+            var enumerator = sources.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                return current;
+            }
+
+            throw new InvalidOperationException($"{nameof(sources)} is empty");
         }
     }
 }
