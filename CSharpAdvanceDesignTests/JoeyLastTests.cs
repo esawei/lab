a1ee9e922df94fd4d46d320a07303772339356cb
Reece Chen <esawei5190@gmail.com>
2020-02-29 12:10:53 +0800

    [feat][last] add test case get_last_chen

--- CSharpAdvanceDesignTests/JoeyLastTests.cs
+++ CSharpAdvanceDesignTests/JoeyLastTests.cs
@@ -23,8 +23,30 @@ namespace CSharpAdvanceDesignTests
 
             var employee = JoeyLast(employees);
 
-            new Employee { FirstName = "Cash", LastName = "Li" }
+            new Employee {FirstName = "Cash", LastName = "Li"}
                 .ToExpectedObject().ShouldMatch(employee);
+        }
+
+        [Test]
+        public void get_last_chen()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "David", LastName = "Chen"},
+                new Employee {FirstName = "Cash", LastName = "Li"},
+            };
+
+            var employee = JoeyLastWithCondition(employees);
+
+            new Employee {FirstName = "David", LastName = "Chen"}
+                .ToExpectedObject().ShouldMatch(employee);
+        }
+
+        private Employee JoeyLastWithCondition(IEnumerable<Employee> employees)
+        {
+            throw new NotImplementedException();
         }
 
         [Test]
